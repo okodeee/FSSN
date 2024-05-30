d4275f345d16999c9e4858676c9050ed24d2038d
okod <okodee@naver.com>
2024-05-30 23:47:03 +0900

    initial setting

---
+++ src/main/java/cse/khu/FSSN/FssnApplication.java
@@ -0,0 +1,13 @@
+package cse.khu.FSSN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FssnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FssnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cse/khu/FSSN/FssnApplicationTests.java
@@ -0,0 +1,13 @@
+package cse.khu.FSSN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FssnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
